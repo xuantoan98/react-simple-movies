e3464a2965c454c508b58ef108eba67f1c1e31e8
toankx <toankx@amirdetail.com>
2022-06-24 01:36:02 +0700

    Update search feature

--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import useSWR from 'swr';
 import MovieCard from '../components/movie/MovieCard';
 import { apiKey, fetcher } from '../config';
+import useDebounce from '../hooks/useDebounce';
 
 const MoviePage = () => {
 
-    const { data } = useSWR(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`, fetcher);
+    const [ filter, setFilter ] = useState("");
+    const [ url, setUrl ] = useState(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`);
+    const filterDebounce = useDebounce(filter, 500);
+    const handleFilterChange = (e) => {
+
+        setFilter(e.target.value);
+    }
+
+    const { data, error } = useSWR(url, fetcher);
+    const loading = !data && !error;
+
+    useEffect(() => {
+        if (filterDebounce) {
+            setUrl(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${filterDebounce}`);
+        } else {
+            setUrl(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`);
+        }
+    }, [filterDebounce]);
+
     const movies = data?.results || [];
 
     return (
         <div className=" py-10">
             <div className="flex mb-10">
                 <div className="flex-1">
-                    <input type="text" className='w-full p-4 bg-slate-800 text-white outline-none' placeholder='Type here to search ...' />
+                    <input 
+                        type="text" 
+                        className='w-full p-4 bg-slate-800 text-white outline-none' 
+                        placeholder='Type here to search ...'
+                        onChange={handleFilterChange} />
                 </div>
 
                 <button className='p-4 bg-primary text-white'>
@@ -22,8 +45,12 @@ const MoviePage = () => {
                 </button>
             </div>
 
+            { loading && (
+                <div className='w-10 h-10 rounded-full border-4 border-primary border-t-transparent border-t-4 mx-auto animate-spin'></div>
+            )}
+
             <div className="grid grid-cols-4 gap-10">
-                {movies.length > 0 && movies.map(item => (
+                { !loading && movies.length > 0 && movies.map(item => (
                     <MovieCard item={item} key={item.id}></MovieCard>
                 ))}
             </div>
